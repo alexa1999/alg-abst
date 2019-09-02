8671eb5ac9baf82c83e25b497cb32de64c5b968e
alexa1999 <anel.veliz@ucsp.edu.pe>
2019-09-02 15:08:31 -0500

    Add files via upload

--- tareaa1/main.cpp
+++ tareaa1/main.cpp
@@ -10,8 +10,8 @@ int main()
 //    Sumi a(mensaje);
 //    string encriptado=a.Encriptar(mensaje);
 //    cout<<"encriptado:"<< encriptado<<endl;
-    string mensaje1="cqs";
-    string clave="cpq";
+    string mensaje1="nm3vnwgr86pzgwcl97611fjoh18etqlf71qh0k0d5sr1fwjhabqw8kc3xsrua1cij8ji4qbvj3fl3vganybnryf6wkvfy63qb zr3701jk0";
+    string clave="g9svou3fu7lhywvm9vyvng8g42nebrlgy1fhjlwwmoszwf24b8dx43uzyani7ju2f955nasrkkwm0rz3ihbkdzbodkw4yv4cc6hd4s0kfiw";
     Sumi b(mensaje);
     string desenc=b.Desencriptar(mensaje1,clave);
     cout<<"encriptado:"<< desenc<<endl;
